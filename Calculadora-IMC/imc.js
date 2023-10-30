b7b434bcc61355686776d42bb169e93364c6cd64
MaolyDevTech <144358009+MaolyDevTech@users.noreply.github.com>
2023-10-30 13:40:35 +0000

    Nova Branches

--- Calculadora-IMC/imc.js
+++ Calculadora-IMC/imc.js
@@ -1,36 +1,41 @@
 const calcular = document.getElementById('calcular');
+const nomeInput = document.getElementById('nome');
+const alturaInput = document.getElementById('altura');
+const pesoInput = document.getElementById('peso');
+const resultado = document.getElementById('resultado');
 
-
-function imc(){
-    const nome = document.getElementById('nome').value;
-    const altura = document.getElementById('altura').value;
-    const peso = document.getElementById('peso').value;
-    const resultado = document.getElementById('resultado');
-
-    if (nome !== '' && altura !== '' && peso !== ''){
-
-        const valorIMC = (peso /(altura * altura)).toFixed(2);
+function calcularIMC(nome, altura, peso) {
+    if (nome !== '' && altura !== '' && peso !== '') {
+        const valorIMC = (peso / (altura * altura)).toFixed(2);
 
         let classificacao = "";
-        
-        if (valorIMC < 18.5){
+
+        if (valorIMC < 18.5) {
             classificacao = 'abaixo do peso. ❗❗';
-        } else if (valorIMC < 25){
+        } else if (valorIMC < 25) {
             classificacao = 'com o peso ideal. Parabéns 🎉✨!!';
-        } else if (valorIMC < 30){
-            classificacao= 'Levemente pasado de peso.';
-        } else if (valorIMC < 35){
-            classificacao= 'com obesidade Grau I.'
-        } else if (valorIMC < 40){
-            classificacao= 'com obesidade Grau II.';
-        } else if (valorIMC < 45){
+        } else if (valorIMC < 30) {
+            classificacao = 'Levemente acima do peso.';
+        } else if (valorIMC < 35) {
+            classificacao = 'com obesidade Grau I.'
+        } else if (valorIMC < 40) {
+            classificacao = 'com obesidade Grau II.';
+        } else if (valorIMC < 45) {
             classificacao = 'com obesidade Grau III. Cuidado ⚠ ❗❗❗❗';
         }
 
-        resultado.textContent = `${nome} seu IMC é ${valorIMC} e você está ${classificacao}`
-
+        resultado.textContent = `${nome} seu IMC é ${valorIMC} e você está ${classificacao}`;
     } else {
-        resultado.textContent = 'Preencha todos os Campos, por gentileza'
+        resultado.textContent = 'Preencha todos os campos, por gentileza';
     }
 }
-calcular.addEventListener('click', imc);
+
+function handleClick() {
+    const nome = nomeInput.value;
+    const altura = parseFloat(alturaInput.value);
+    const peso = parseFloat(pesoInput.value);
+
+    calcularIMC(nome, altura, peso);
+}
+
+calcular.addEventListener('click', handleClick);
